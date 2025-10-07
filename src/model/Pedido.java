b0961b4a2139953ed565b1a1de4e2b3a3f3ace5c
Danil0Ribeir0 <danilo.ribeiro18rs@gmail.com>
2025-10-07 08:55:52 -0300

    models v1

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,5 +1,49 @@
 package model;
 
 public class Pedido {
-
+	public int id;
+	public Cliente cliente;
+	public String dataHora;
+	public Restaurante restaurante;
+	public String status;
+	public Pedido(int id, Cliente cliente, String dataHora, Restaurante restaurante, String status) {
+		super();
+		this.id = id;
+		this.cliente = cliente;
+		this.dataHora = dataHora;
+		this.restaurante = restaurante;
+		this.status = status;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	public String getDataHora() {
+		return dataHora;
+	}
+	public void setDataHora(String dataHora) {
+		this.dataHora = dataHora;
+	}
+	public Restaurante getRestaurante() {
+		return restaurante;
+	}
+	public void setRestaurante(Restaurante restaurante) {
+		this.restaurante = restaurante;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
 }
