ed3a0045b470b54b7e98d50e39ce9a89bb25b8cc
naok1m <thiagonaoki2013@gmail.com>
2025-10-23 08:56:02 -0300

    services Cliente

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -1,5 +1,19 @@
 package service;
 
-public class ClienteService {
+import dao.ClienteDAO;
+import model.Cliente;
 
+public class ClienteService {
+	private ClienteDAO clienteDAO;
+	
+	
+	public ClienteService() {
+		this.clienteDAO = new ClienteDAO();
+	}
+	public void cadastrarCliente(Cliente cliente) {
+		if (cliente.getNome() == null || cliente.getNome().isBlank()) {
+			throw new IllegalArgumentException("Nome obrigatorio");
+		}
+		clienteDAO.inserirCliente(cliente);
+	}
 }
