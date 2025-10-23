893ecf603bbfe7c8dc55a8b0f6fcf87e994d696b
naok1m <thiagonaoki2013@gmail.com>
2025-10-23 09:01:46 -0300

    service pedido v1.0

--- src/service/PedidoService.java
+++ src/service/PedidoService.java
@@ -1,5 +1,13 @@
 package service;
 
 public class PedidoService {
+	// verificar se o cliente existe
 
+	// verficar se os produtos existem
+	
+	// calcular valor total
+	
+	// gravar pedido e items
+	
+	// atualizar status
 }
